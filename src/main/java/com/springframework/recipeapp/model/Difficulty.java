00a8eedb4eb1bcc63683db8c3f16057403526524
devdpm <jansen.dpm@gmail.com>
2023-01-14 09:44:13 +0100

    Updated difficulty to enumerable, relation with String

--- src/main/java/com/springframework/recipeapp/model/Difficulty.java
+++ src/main/java/com/springframework/recipeapp/model/Difficulty.java
@@ -1,25 +1,7 @@
 package com.springframework.recipeapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 enum Difficulty {
     EASY,
     MEDIUM,
-    HARD;
-
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    HARD
 }
--- src/main/java/com/springframework/recipeapp/model/Recipe.java
+++ src/main/java/com/springframework/recipeapp/model/Recipe.java
@@ -24,7 +24,7 @@ public class Recipe {
     @Lob // BLOB
     private Byte[] image;
 
-    @OneToOne
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToOne (cascade = CascadeType.ALL)
